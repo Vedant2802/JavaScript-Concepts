31e3660a66d2c0b59b7b00e0052e1a0b13747307
vedant-akshat <103035956+vedant-akshat@users.noreply.github.com>
2025-01-14 02:14:53 +0530

    File Structure change

---
+++ EventsJS/event.js
@@ -0,0 +1,11 @@
+document.querySelector("#grandparent").addEventListener("click", function () {
+  console.log("grandparent CLicked");
+});
+
+document.querySelector("#parent").addEventListener("click", function () {
+  console.log("parent CLicked");
+});
+
+document.querySelector("#children").addEventListener("click", function () {
+  console.log("children Clicked");
+});
