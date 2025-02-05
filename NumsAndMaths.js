6210d599ff707f8d2aa9e1919b2fee748966643f
vedant-akshat <103035956+vedant-akshat@users.noreply.github.com>
2025-02-06 02:13:45 +0530

    Revision of JS

---
+++ NumsAndMaths.js
@@ -0,0 +1,10 @@
+const score = 400;
+
+const balance = new Number(400);
+
+const myName = balance.toString();
+// console.log(typeof myName);
+
+// Maths library comes inbuilt with JS
+
+console.log(Math);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,28 @@
+const myObj = {
+  name: "akshat",
+  age: 24,
+  email: "akshat.vedant@google.com",
+  home: "Muz",
+};
+
+// Methods to access an object
+
+console.log(myObj["name"]);
+
+// 2nd method
+
+console.log(myObj.name);
+
+// how to add a symbol to an object
+
+const newSymbol = Symbol("key");
+
+const myObj2 = {
+  name: "akshat",
+  age: 24,
+  email: "akshat.vedant@google.com",
+  home: "Muz",
+  [newSymbol]: 24,
+};
+
+console.log(typeof myObj2["newSymbol"]);
