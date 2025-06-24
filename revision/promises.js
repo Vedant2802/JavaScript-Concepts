a7f693341e003cb2d835569f3834828617c57eaf
Vedant2802 <avedant981@gmail.com>
2025-06-25 03:41:21 +0530

    promises revision

---
+++ revision/promises.js
@@ -0,0 +1,133 @@
+// const promise = new Promise(function (resolve, reject) {});
+
+// Executor function
+// resolve and reject are callbacks give to us by javascript itself
+
+/*
+    function (resolve, reject) {}
+*/
+
+// states
+// pending , fulfilled , rejected
+
+// result -
+// undefined : initially when in pending state
+// value : when resolved
+// error : when rejected
+
+// let promise = new Promise(function (resolve, reject) {
+//   resolve("Hey I am done");
+// });
+
+// let promise2 = new Promise(function (resolve, reject) {
+//   reject("Hey I am not done");
+// });
+
+// Handling Promises
+
+// .then()
+// const promise = new Promise(function (resolve, reject) {
+//   resolve("I have resolved this");
+//     reject("I am rejected");
+// });
+
+// promise.then(
+//   (result) => {
+//     console.log(result);
+//   },
+//   (error) => {
+//     console.error(error);
+//   }
+// );
+// promise
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// promise
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   })
+//   .finally(() => {
+//     console.log("finally called");
+//   });
+// .catch()
+// .finally()
+
+/*
+  Some Must remember rules 
+
+  1. Every Resolved Promise gives you a .then() handler , every Rejected Promise gives a .catch() handler
+
+  2. You can mainly do 3 important things from the .then() method , You can return any other promise , 
+  (asynchronous) , you can return a resolved value , You can throw an error
+
+  3. unlike .then() and .catch() , the .finally() , handler does not process the result value or error , It just passes the result as is to the next handler
+
+  4. calling a .then() handler multiple times on a single promise is not chaining
+
+*/
+
+// Return a promise from the .then() handler
+
+// let getUser = new Promise(function (resolve, reject) {
+//   const user = {
+//     name: "John",
+//     email: "john.com",
+//     password: "john@123",
+//   };
+//   resolve(user);
+// });
+
+// getUser
+//   .then((result) => {
+//     console.log(`Got user ${result.name}`);
+//     return new Promise(function (resolve, reject) {
+//       setTimeout(() => {
+//         resolve("New Delhi");
+//       }, 2000);
+//     });
+//   })
+
+//   .then((res) => {
+//     console.log(`The address is ${res}`);
+//   });
+
+// Rule 3 example
+
+// let promiseFinally = new Promise(function (resolve, reject) {
+//   resolve("Testing Finally...");
+// });
+
+// promiseFinally
+//   .finally(() => {
+//     console.log("running finally()...");
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   });
+
+// Rule 4 example
+
+// let promise = new Promise(function (resolve, reject) {
+//   //   this.value = 99;
+//   resolve(this);
+// });
+
+// promise
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// promise.then((res) => {
+//   console.log(res);
+// });
